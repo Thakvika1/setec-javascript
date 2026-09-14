5d7472cf70950d729ce4d9e7b7112d3fe0320b1d
Thakvika1 <thakvika.thorn@gmail.com>
2026-09-14 13:19:33 +0700

    operator

---
+++ operator.js
@@ -0,0 +1,19 @@
+
+// cannot use with (- , *, /)
+// let a = 8;
+// let b = "8hi";
+// let sum = a + b; // string concatenation
+// console.log(sum);
+
+
+// let a = 8;
+// let b = "8hi";
+// let  sum = a - b;
+// console.log(sum); // NaN
+
+const a = 9;
+const b = 7;
+console.log("Sum of a and b is: ${a + b}");
+console.log('Sum of a and b is: ${a + b}');
+// backtick
+console.log(`Sum of a and b is: ${a + b}`);
---
+++ typeof.js
@@ -0,0 +1,13 @@
+
+let a = 9; // number
+a = "hello"; // string
+a = 76.45; // number
+a = true; // boolean
+a = null; // object
+
+a = [1, 2, 3, 4];
+
+
+console.log(typeof a);
+
+// console.log(a);
